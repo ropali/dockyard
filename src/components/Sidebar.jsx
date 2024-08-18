0ea4fc18f28e5d64253be7822bf630a1a406e549
ropali <ropali68@gmail.com>
2024-08-18 18:44:53 +0530

    added new logo

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -9,8 +9,8 @@ function Sidebar() {
     <div className="flex h-screen w-16 flex-col justify-between bg-base-100 z-50">
       <div>
         <div className="inline-flex size-16 items-center justify-center">
-          <span className="grid size-10 place-content-center rounded-lg bg-base-300 text-xs text-base-100">
-            DD
+          <span className="grid size-10 place-content-center rounded-lg">
+            <img src="/circle_logo.png" alt="" srcset="" />
           </span>
         </div>
 

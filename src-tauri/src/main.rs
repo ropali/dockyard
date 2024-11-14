8e2ec9d69d3b82f3c71da25e0483c1c2760288bd
ropali <ropali68@gmail.com>
2024-11-14 17:22:44 +0530

    feat: Container export option added

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -1,15 +1,12 @@
 // Prevents additional console window on Windows in release, DO NOT REMOVE!!
 #![cfg_attr(not(debug_assertions), windows_subsystem = "windows")]
 
-use crate::commands::container::{
-    container_operation, container_stats, fetch_container_info, fetch_containers, get_container,
-    rename_container, stream_docker_logs,
-};
+use crate::commands::container::{container_operation, container_stats, export_container, fetch_container_info, fetch_containers, get_container, rename_container, stream_docker_logs};
 use crate::commands::extra::{cancel_stream, get_version, ping};
 use crate::commands::image::{delete_image, export_image, image_history, image_info, list_images};
 use crate::commands::network::{inspect_network, list_networks};
-use crate::commands::volume::{inspect_volume, list_volumes};
 use crate::commands::terminal::get_available_terminals;
+use crate::commands::volume::{inspect_volume, list_volumes};
 use crate::state::AppState;
 use crate::utils::storage::setup_storage;
 
@@ -51,7 +48,8 @@ fn main() {
             get_version,
             ping,
             get_available_terminals,
-            rename_container
+            rename_container,
+            export_container,
         ])
         .run(tauri::generate_context!())
         .expect("error while running tauri application");
--- src/Icons/index.jsx
+++ src/Icons/index.jsx
@@ -491,3 +491,19 @@ export function IconCancel(props) {
         </svg>
     )
 }
+
+
+export function IconThreeDots(props) {
+    return (
+        <svg
+            fill="currentColor"
+            viewBox="0 0 16 16"
+            height="1em"
+            width="1em"
+            {...props}
+        >
+            <path
+                d="M3 9.5a1.5 1.5 0 110-3 1.5 1.5 0 010 3zm5 0a1.5 1.5 0 110-3 1.5 1.5 0 010 3zm5 0a1.5 1.5 0 110-3 1.5 1.5 0 010 3z"/>
+        </svg>
+    );
+}

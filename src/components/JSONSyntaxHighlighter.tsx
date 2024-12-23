c52540f72a67abacd820006427f622770f51c415
ropali <ropali68@gmail.com>
2024-12-23 22:19:56 +0530

    fix: json syntax highlighter component type issue on compilation

--- src/components/JSONSyntaxHighlighter.tsx
+++ src/components/JSONSyntaxHighlighter.tsx
@@ -1,29 +1,32 @@
 import React from 'react';
-import { PrismLight as SyntaxHighlighter } from 'react-syntax-highlighter';
-import { nord as theme } from 'react-syntax-highlighter/dist/esm/styles/prism';
+import {PrismLight as SyntaxHighlighter} from 'react-syntax-highlighter';
+import {nord as theme} from 'react-syntax-highlighter/dist/esm/styles/prism';
 import json from 'react-syntax-highlighter/dist/esm/languages/prism/json';
 
+// Register the JSON language
 SyntaxHighlighter.registerLanguage('json', json);
 
 export interface JSONSyntaxHighlighterProps {
-  id: string;
-  json: string;
+    id: string;
+    json: string;
 }
 
-export default function JSONSyntaxHighlighter({json }: JSONSyntaxHighlighterProps) {
-  return (
-    <div className={`h-full flex flex-col h-full`}>
-      <SyntaxHighlighter
-        language="json"
-        style={theme}
-        className="flex-grow overflow-auto"
-        customStyle={{
-          margin: 0,
-          height: '100%',
-        }}
-      >
-        {JSON.stringify(json, null, 2)}
-      </SyntaxHighlighter>
-    </div>
-  );
+const SyntaxHighlighterComponent = SyntaxHighlighter as any;
+
+export default function JSONSyntaxHighlighter({json}: JSONSyntaxHighlighterProps) {
+    return (
+        <div className="h-full flex flex-col">
+            <SyntaxHighlighterComponent
+                language="json"
+                style={theme}
+                className="flex-grow overflow-auto"
+                customStyle={{
+                    margin: 0,
+                    height: '100%',
+                }}
+            >
+                {JSON.stringify(json, null, 2)}
+            </SyntaxHighlighterComponent>
+        </div>
+    );
 }
